918b3de5614ac72543732100a10e98e8fff70b2c
hthuynh <mich.hthuynh@gmail.com>
2020-06-04 17:54:29 +0700

    update

--- be/app.js
+++ be/app.js
@@ -2,18 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-const auth_routes = require('./routes/auth.js') 
 const bodyParser = require('body-parser')
+
+const authRoutes = require('./routes/auth.route.js')
+const productsRoutes = require('./routes/products.js') 
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use('/auth', auth_routes)
+app.use('/auth', authRoutes)
+app.use('/products', productsRoutes)
 
-app.post('/test', (req, res) =>{
-    const {email} = req.body
-    console.log(email)
-})
 
 app.listen(3000, () => {
     console.log(`Server is running at ${PORT}`)
--- be/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/auth.controller')
-
-
-router.get('/', controller.get)
-router.post('/', controller.index)
-
-module.exports = router
